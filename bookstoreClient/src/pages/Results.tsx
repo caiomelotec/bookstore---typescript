6176fddaf794c6195c3647927b1aca351cc3e039
caiomelotec <caio.melotec@icloud.com>
2023-09-16 22:21:31 +0200

    link to bookId

--- bookstoreClient/src/pages/Results.tsx
+++ bookstoreClient/src/pages/Results.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { useResults } from "../contex/ResultsContext";
 import "../styles/Results.css";
 import { formatCurrency } from "../ultilities/formatCurrency";
@@ -10,23 +11,25 @@ export const Results = () => {
       <h2 className="results-h2">Results: {results.length}</h2>
       <div className="wrapper-results-container">
         {results.map((book) => (
-          <div key={book.id} className="results-container">
-            <img className="book-img" src={book.imgUrl} alt="" />
-            <div className="book-info">
-              <p className="book-title">{book.title}</p>
-              <p className="book-author">{book.author}</p>
-              <p className="book-price">{formatCurrency(book.price)}</p>
-              <div>
-                <span>Delivery</span>
-                <span>
-                  <BsFillCircleFill
-                    size={8}
-                    fill={book.delivery ? "green" : "red"}
-                  />
-                </span>
+          <Link key={book.id} to={`/bookdetail/${book.id}`}>
+            <div className="results-container">
+              <img className="book-img" src={book.imgUrl} alt="" />
+              <div className="book-info">
+                <p className="book-title">{book.title}</p>
+                <p className="book-author">{book.author}</p>
+                <p className="book-price">{formatCurrency(book.price)}</p>
+                <div>
+                  <span>Delivery</span>
+                  <span>
+                    <BsFillCircleFill
+                      size={8}
+                      fill={book.delivery ? "green" : "red"}
+                    />
+                  </span>
+                </div>
               </div>
             </div>
-          </div>
+          </Link>
         ))}
       </div>
     </>
